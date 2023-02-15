8c119700b71037f556f97339d79d0eff9a2a4b69
aLVINlEE9 <fldh3746@gmail.com>
2023-02-16 03:45:07 +0900

    Fix: absl library issue solved
    brew 로 통일

--- live-streaming-server/client/grpc/grpc_setting.sh
+++ live-streaming-server/client/grpc/grpc_setting.sh
@@ -1,11 +1,8 @@
-git clone --recurse-submodules -b v1.50.0 --depth 1 --shallow-submodules https://github.com/grpc/grpc
-chmod 777 -R grpc
-wget -c https://github.com/protocolbuffers/protobuf/releases/download/v21.12/protobuf-cpp-3.21.12.tar.gz -O - | tar -xz
-# !! before protoc ... you should do following steps to build !!
-# $ cd grpc
-# $ mkdir -p cmake/build
-# $ cd cmake/build
-# $ cmake ../..
-# $ make -j4
-# $ sudo make install
+# you should download requirements
+# @ brew install protobuf
+# @ brew install opencv
+# @ brew install boost
+# @ brew install abseil
+# @ brew install grpc
+# @ brew install re2
 protoc -I=./ --cpp_out=./ --grpc_out=./ --plugin=protoc-gen-grpc=`which grpc_cpp_plugin` ./stream_service.proto
