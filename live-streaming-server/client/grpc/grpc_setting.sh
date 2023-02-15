f1e3202157123f416d7ae0d7322118da381b3e5a
aLVINlEE9 <fldh3746@gmail.com>
2023-02-15 17:55:50 +0900

    Fix: grpc setting 스크립트 수정

--- live-streaming-server/client/grpc/grpc_setting.sh
+++ live-streaming-server/client/grpc/grpc_setting.sh
@@ -1,2 +1,10 @@
+git clone --recurse-submodules -b v1.50.0 --depth 1 --shallow-submodules https://github.com/grpc/grpc
+chmod 777 -R grpc
 wget -c https://github.com/protocolbuffers/protobuf/releases/download/v21.12/protobuf-cpp-3.21.12.tar.gz -O - | tar -xz
-protoc -I=./ --cpp_out=./ ./stream_service.proto
+# !! before protoc ... you should do following steps to build !!
+# $ cd grpc
+# $ mkdir -p cmake/build
+# $ cmake ../..
+# $ make -j4
+# $ sudo make install
+protoc -I=./ --cpp_out=./ --grpc_out=./ --plugin=protoc-gen-grpc=`which grpc_cpp_plugin` ./stream_service.proto
